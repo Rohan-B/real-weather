c3624fdf91bb02578480f4fa179ecbc4f0c75893
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-11 19:23:26 -0800

    error on jsonp?

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var myApp = angular.module('weather', []);
 myApp.controller('WeatherController', [function($scope, $http) {
 	$scope.currWeather = "hi";
 	
-	$http.jsonp("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
+	$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
 			console.log("get here");
 			console.log(response);
-			$scope.currWeather = response.forecast.txt_forecast; 
+			//$scope.currWeather = response.forecast.txt_forecast; 
 		});
 }]);
