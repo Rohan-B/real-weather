be43cdf097bf15a860aff50dc3e3a90bca508721
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-11 19:57:39 -0800

    testing scope

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var myApp = angular.module('weather', []);
 
 myApp.controller('WeatherController', function($scope, $http) {
 	$scope.currWeather = "hi";
-	
+	$scope.test = "willthiswork";
+
 	$http.jsonp("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
 			console.log("get here");
