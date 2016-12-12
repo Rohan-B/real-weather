4c4a49d1bbb76dd76bdf689a807272dc4259a8ad
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-11 19:11:27 -0800

    bring back api call

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ myApp.controller('WeatherController', [function($scope, $http) {
 	$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
 			console.log("get here");
-			//$scope.currWeather = response.forecast.txt_forecast.forecastday[0].fctext; 
+			$scope.currWeather = response.forecast.txt_forecast.forecastday[0].fctext; 
 		});
 }]);
