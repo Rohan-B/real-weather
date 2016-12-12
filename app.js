55ef4290ca3387a14a9029d0faa425c0258b400a
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-11 19:06:06 -0800

    testing

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var myApp = angular.module('weather', []);
 
 myApp.controller('WeatherController', ['$scope', '$http', function($scope, $http) {
 	$scope.currWeather = "hi";
-
+	
 	$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
-			$scope.currWeather = response.forecast.txt_forecast.forecastday[0].fctext; 
+			console.log("get here");
+			//$scope.currWeather = response.forecast.txt_forecast.forecastday[0].fctext; 
 		});
 }]);
