ffd0b4804150070dac39d4a239d98af5fca11f00
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-11 19:22:05 -0800

    troubleshoot specific weather

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ myApp.controller('WeatherController', [function($scope, $http) {
 	$http.jsonp("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
 			console.log("get here");
-			$scope.currWeather = response.forecast.txt_forecast.forecastday[0].fctext; 
+			console.log(response);
+			$scope.currWeather = response.forecast.txt_forecast; 
 		});
 }]);
