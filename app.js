5eda16a9cdeb1e3575900fc1f6a48213a6b4039a
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-12 17:33:34 -0800

    tryna fix cross origin request issue

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ myApp.controller('WeatherController', function($scope, $http) {
 	$scope.currWeather = "hi";
 	$scope.test = "willthiswork";
 
-	$http.jsonp("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json&callback=JSON_CALLBACK")
-	  .success(function(response){ 
+	$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
+	  .then(function(response){ 
 			console.log("get here");
 			console.log(response);
 			$scope.currWeather = response.data.forecast.txt_forecast.forecastday[0].fcttext; 
