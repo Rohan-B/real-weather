a92f2f215ca0fd9ff97ba0b17abdac5dd5519789
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-12 19:13:45 -0800

    parse to int first

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ myApp.controller('WeatherController', function($scope, $http) {
 			$scope.currWeather = response.data.forecast.txt_forecast.forecastday[0].fcttext; 
 			console.log(response.data);	
 			//get the current high and the low for the weather 
-			currHigh = response.data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
-			currLow = response.data.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+			currHigh = parseInt(response.data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+			currLow = parseInt(response.data.forecast.simpleforecast.forecastday[0].low.fahrenheit);
 			//get the current weather average 
 			
-			currAverage = currHigh + currLow;
-			console.log(currAverage /2);
-			console.log((currHigh + currLow)/2);
+			currAverage = (currHigh + currLow)/2;
+			console.log(currAverage);
 			
 			$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/yesterday/q/CA/San_Francisco.json")
 			.then(function(response){ 
