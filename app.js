c1fe016bde8a26a3216f35c1ba9415ad953ed34c
Rohan Bhargava <rohan.bhargava@gmail.com>
2016-12-12 18:35:06 -0800

    check out yesterdays data

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var myApp = angular.module('weather', []);
 
 myApp.controller('WeatherController', function($scope, $http) {
-	$scope.currWeather = "hi";
-	$scope.test = "willthiswork";
 
 	$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/forecast/q/CA/San_Francisco.json")
 	  .then(function(response){ 
-			console.log("get here");
 			console.log(response);
 			$scope.currWeather = response.data.forecast.txt_forecast.forecastday[0].fcttext; 
-			console.log(currWeather);
+			$scope.currHigh = response.data.forecast.simpleforecastforecastday[0].high.fahrenheit;
+			$scope.currLow = response.data.forecast.simpleforecastforecastday[0].low.fahrenheit;
+			$scope.currAverage = ($scope.currHigh + $scope.currLow)/2;
+		})
+		.catch(function(data){
+			console.log(data);
 		});
+
+$http.get("http://api.wunderground.com/api/d2c25648f13d77b5/yesterday/q/CA/San_Francisco.json")
+	  .then(function(response){ 
+			console.log(response);
+		})
+		.catch(function(data){
+			console.log(data);
+		});
+
+
+
+
+
 });
+
+
